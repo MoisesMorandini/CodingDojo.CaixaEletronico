b2e00e56e082270c45793d24ffb88cd0e322f125
Lazaro Menezes <lazaro.menezes@actminds.com>
2020-11-27 09:59:48 -0300

    Initial commit

---
+++ CodingDojo.CaixaEletronico.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CodingDojo.CaixaEletronico.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(false);
+        }
+    }
+}
---
+++ CodingDojo.CaixaEletronico/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CodingDojo.CaixaEletronico
+{
+    public class Class1
+    {
+    }
+}
